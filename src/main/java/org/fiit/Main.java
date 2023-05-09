b53d256e7614e033be62dfac4079dca8b463c3e9
Withalion <bagarmatej@gmail.com>
2023-05-09 09:30:23 +0200

    working solution, results are bit worse than best scenario

--- src/main/java/org/fiit/Main.java
+++ src/main/java/org/fiit/Main.java
@@ -1,5 +1,26 @@
-package org.fiit;public class Main {
+package org.fiit;
+
+import java.util.List;
+
+public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        CalculateController calculateController = new CalculateController();
+        System.out.println("Generating Cayley graphs:");
+
+        for (int n = 5; n <= 50; n++) {
+            System.out.println(">--------------------------------------<");
+            System.out.println("Calculating smallest d for n = " + n);
+            List<Integer> generators = calculateController.calculate(n);
+            if (!generators.isEmpty()) {
+                System.out.println("Generators for this graph are: " + generators);
+            }
+            else {
+                System.out.println("Graph couldn't be constructed");
+            }
+        }
     }
+
+    private Main() {}
 }
